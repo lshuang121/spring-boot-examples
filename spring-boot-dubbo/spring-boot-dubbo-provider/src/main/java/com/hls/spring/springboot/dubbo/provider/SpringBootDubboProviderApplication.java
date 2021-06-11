730ceab5c7b3ab5efd84ad4a9166ef7eb375c3c5
lshuang121 <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>
2021-06-11 12:21:48 -0500

    implement dubbo provider

--- spring-boot-dubbo/spring-boot-dubbo-provider/src/main/java/com/hls/spring/springboot/dubbo/provider/SpringBootDubboProviderApplication.java
+++ spring-boot-dubbo/spring-boot-dubbo-provider/src/main/java/com/hls/spring/springboot/dubbo/provider/SpringBootDubboProviderApplication.java
@@ -1,10 +1,11 @@
 package com.hls.spring.springboot.dubbo.provider;
 
-import com.hls.spring.common.model.User;
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableDubbo(scanBasePackages = "com.hls.spring")
 public class SpringBootDubboProviderApplication {
 
   public static void main(String[] args) {
---
+++ spring-boot-dubbo/spring-boot-dubbo-provider/src/main/java/com/hls/spring/springboot/dubbo/provider/service/UserServiceImpl.java
@@ -0,0 +1,17 @@
+package com.hls.spring.springboot.dubbo.provider.service;
+
+import com.hls.spring.common.contract.IUserService;
+import com.hls.spring.common.model.User;
+import org.apache.dubbo.config.annotation.DubboService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@DubboService(version = "${user.service.version}")
+public class UserServiceImpl implements IUserService {
+  private static final Logger log = LoggerFactory.getLogger(UserServiceImpl.class);
+
+  @Override
+  public User getUserById(int uid) {
+    return null;
+  }
+}
