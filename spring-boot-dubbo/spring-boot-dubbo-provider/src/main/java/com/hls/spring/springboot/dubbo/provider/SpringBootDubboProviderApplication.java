055f064e2a05467564839c0df6869686e6a8838e
lshuang121 <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>
2021-06-11 11:25:30 -0500

    change relative path

---
+++ spring-boot-dubbo/spring-boot-dubbo-provider/src/main/java/com/hls/spring/springboot/dubbo/provider/SpringBootDubboProviderApplication.java
@@ -0,0 +1,13 @@
+package com.hls.spring.springboot.dubbo.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDubboProviderApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootDubboProviderApplication.class, args);
+  }
+
+}
---
+++ spring-boot-dubbo/spring-boot-dubbo-provider/src/test/java/com/hls/spring/springboot/dubbo/provider/SpringBootDubboProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hls.spring.springboot.dubbo.provider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDubboProviderApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
